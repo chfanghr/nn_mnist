d5e99e06e0b8a7c6684b3b7b1feddf04bcbc59e6
fanghr <chfanghr@gmail.com>
2019-12-13 02:52:59 +0800

    update build script

--- src/wasm/build.sh
+++ src/wasm/build.sh
@@ -10,4 +10,3 @@ fi
 em++ -s WASM=1 --embed-file "${PROJECT_ROOT}"/data/network.dat@network.dat \
   "${PROJECT_ROOT}"/src/wasm/main.cc "${PROJECT_ROOT}"/src/nn_mnist/network.cc \
   -I "${PROJECT_ROOT}"/include -o wasm_nn_mnist.js --std=c++17
-
